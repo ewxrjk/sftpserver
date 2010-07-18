bde70c53d8d08598464dfe463ef5fda5518b4d30
Richard Kettlewell <rjk@greenend.org.uk>
2010-07-18 17:34:23 +0100

    Rename prepare to autogen.sh, & cope with silly automake names.

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,22 @@
 
 set -e
 set -x
+
+# Find an automake
+if [ -z "$AUTOMAKE" ]; then
+  for prog in automake automake-1.10 automake-1.9 automake-1.8 automake-1.7; do
+    if type $prog >/dev/null 2>&1; then
+      AUTOMAKE=$prog
+      break
+    fi
+  done
+  if [ -s "$AUTOMAKE" ]; then
+    echo "ERROR: no automake found" >&2
+    exit 1
+  fi
+fi
+ACLOCAL=${AUTOMAKE/automake/aclocal}
+
 srcdir=$(dirname $0)
 here=$(pwd)
 cd $srcdir
@@ -15,15 +31,15 @@ if [ ! -e COPYING ]; then
   done
 fi
 if test -d $HOME/share/aclocal; then
-  aclocal --acdir=$HOME/share/aclocal
+  ${ACLOCAL} --acdir=$HOME/share/aclocal
 else
-  aclocal
+  ${ACLOCAL}
 fi
 grep ^AC_PROG_LIBTOOL configure.ac && libtoolize
 mkdir -p config.aux
 autoconf
 autoheader
-automake -a || true		# for INSTALL
-automake --foreign -a
+${AUTOMAKE} -a || true		# for INSTALL
+${AUTOMAKE} --foreign -a
 cd "$here"
 $srcdir/configure -C "$@" --sysconfdir=/etc --localstatedir=/var
