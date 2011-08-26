cdc9684ad917f99c5f8ed9f4123d76a838d68954
Richard Kettlewell <rjk@terraraq.org.uk>
2011-08-26 19:57:59 +0100

    admin: copyright dates, source url, feedback instructions

--- sftpserver.c
+++ sftpserver.c
@@ -1,6 +1,6 @@
 /*
  * This file is part of the Green End SFTP Server.
- * Copyright (C) 2007 Richard Kettlewell
+ * Copyright (C) 2007, 2009, 2011 Richard Kettlewell
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
